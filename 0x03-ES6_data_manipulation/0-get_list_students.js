6580d40ba8875003bf2486fa27f4e951bbd17b3a
anassch <anaschtioui65@gmail.com>
2024-07-30 13:49:01 +0000

    first task

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,5 @@
+const getListStudents = () => [
+	{ id: 1, firstName: 'Guillaume', location: 'San Fransisco' },
+	{ id: 2, firstName: 'James', location: 'Columbia' },
+	{ id: 5, firstName: 'Serena', location: 'San Fransisco'  }
+];
