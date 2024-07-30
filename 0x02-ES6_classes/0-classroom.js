5a2dcb38e668c4ea33d9eac0b7cbe9664b7740dd
anassch <anaschtioui65@gmail.com>
2024-07-30 14:00:18 +0000

    fix eslint

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,6 +1,6 @@
 // classroom class
 export default class ClassRoom {
-	constructor(maxStudentsSize) {
-		this._maxStudentsSize = maxStudentsSize;
-	}
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
