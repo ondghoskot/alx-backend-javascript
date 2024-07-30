94629a6fdce36351d95b6db185b9b1fa8bf0a2a1
anassch <anaschtioui65@gmail.com>
2024-07-30 22:28:49 +0000

    Typed array

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,12 @@
+const createInt8TypedArray = (length, position, value) => {
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer);
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  view.setInt8(position, value);
+  return buffer;
+};
+
+export default createInt8TypedArray;
