a9b9852dd386bd921021798eabaab395b73e914e
Anass <anaschtioui65@gmail.com>
2024-10-13 17:18:02 +0100

    stdin fixed

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,7 +2,9 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
-  process.stdin.end();
+  if (process.stdin.isTTY) {
+    process.exit();
+  }
 });
 
 process.stdin.on('end', () => {
